4d22ee75556be2029dfd002beaf5054247ed12e9
Henry Christensen <henry_christensen@hotmail.com>
2014-05-25 14:10:25 +0200

    ....

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setinverse <- function(solve) m <<- solve
     getinverse <- function() m
-    list(set = set, get = get,
+    list(set = set, get = get, 
          setinverse = setinverse,
          getinverse = getinverse)
   }
